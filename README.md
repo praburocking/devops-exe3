f0ca1bffbf586e87923a31549e18e0e09e103dc5
praburocking <prabumohan96@gmail.com>
2022-11-12 17:38:24 +0200

    screenshots and text answers added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 program files are found in exe3 folder.
+
+Information about the host and docker version is available as a screenshot parrallel to this file.
+
+The few area where topic based communication can have advantage over HTTP communication are 
+
+information broadcasting-- information can be sent to multiple clients based on the subscribed topic.
+asynchronous process --- the producer dont need to wait for the acknowledgment from the client the message broker (rabbitmq) takes care of it.
+
+
--- exe3/observ/observ.py
+++ exe3/observ/observ.py
@@ -3,6 +3,7 @@ import pika
 import sys
 from datetime import datetime
 import time
+import functools
 
 
 HOST="exe3-rabitmq-1"
@@ -10,7 +11,10 @@ ROUTING_KEY1="compse140.o"
 ROUTING_KEY2="compse140.i"
 EXCHANGE='topic_msg'
 FILE_PATH="/usr/data/temp_file.txt"
-temp_counter=1
+
+# using 'dict' instead of simple 'int', as 'dict' will be defaultly passed as call by value to the function 
+temp_counter={'counter':0}
+
 
 def is_port_in_use(port: int) -> bool:
     import socket
@@ -45,17 +49,19 @@ with open(FILE_PATH, 'w') as fp:
 
 
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body,temp_counter):
     temp_file= open(FILE_PATH, "a",encoding='utf-8')	
     dt = datetime.now()
-    #temp_counter=temp_counter+1	
-    temp_str=str(dt)+" "+str(temp_counter)+" "+str(body)+" "+method.routing_key+"\n"
+    temp_counter['counter']=temp_counter['counter']+1	
+    temp_str=str(dt)+" "+str(temp_counter['counter'])+" "+str(body)+" "+method.routing_key+"\n"
     temp_file.write(temp_str)
     print("********"+temp_str)    
     temp_file.close()
 
+
+on_message_callback = functools.partial(callback, temp_counter=(temp_counter))
 channel.basic_consume(
-    queue='#', on_message_callback=callback, auto_ack=True)
+    queue='#', on_message_callback=on_message_callback, auto_ack=True)
 
 channel.start_consuming()
 
